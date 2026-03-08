69cf4ace6c35692c80752d508258e5c21b94acd8
chawoong <chawoong@umich.edu>
2026-03-08 04:09:59 -0400

    added sitemap from astro, but excluded faq, about, and contact pages since they are not accessible currently

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://koos-salon.com',
+  integrations: [sitemap({
+    filter: (page) => !['https://koos-salon.com/about/', 'https://koos-salon.com/faq/', 'https://koos-salon.com/contact/'].includes(page)
+  })]
+});
