1db4565894b0f1d832075e09d6e75894f0cf5b5f
chawoong <chawoongkoo@gmail.com>
2026-05-11 02:32:34 -0400

    upgraded astro version and readded vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://koos-salon.com',
+
   integrations: [sitemap({
     filter: (page) => !['https://koos-salon.com/about/', 'https://koos-salon.com/faq/', 'https://koos-salon.com/contact/'].includes(page)
-  })]
+  })],
+
+  adapter: vercel()
 });
